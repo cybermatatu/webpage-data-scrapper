dd260e20b2d2c72bcb2fdcd361b06d137aa0747f
Fredrick Njenga <cybermatatu@gmail.com>
2015-07-17 15:23:16 +0300

    Modified Symbol Query Frequency

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     setInterval(function() {
        $.ajax({
-         url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22USDKES%22,%20%22EURKES%22,%20%22GBPKES%22,%20%22JPYKES%22%29&format=json&env=store://datatables.org/alltableswithkeys',
+         url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22USDKES%22,%20%22EURKES%22,%20%22CHFKES%22,%20%22CNYKES%22,%20%22GBPKES%22,%20%22UGXKES%22,%20%22ZARKES%22,%20%22JPYKES%22%29&format=json&env=store://datatables.org/alltableswithkeys',
          dataType: 'json',
          cache: true,
          timeout: 30000,
